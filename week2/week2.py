ffa2903d063a018b29207c30724498784f69ba2e
Rahul Shah <rahuldshah3@gmail.com>
2018-10-24 02:31:46 -0400

    add week2

---
+++ week2/week2.py
@@ -0,0 +1,74 @@
+##y = 74
+##if y == 1:
+##    print("y = 1")
+##elif y == 72:
+##    print("y equals 72")
+##elif y == 74:
+##    print("y equals 74")
+##elif y== 2:
+##    print("yo")
+##else:
+##    print("y is not equal to 1 or 72 or 74 ")
+##print(7)
+##
+##input = -6
+##
+##if(input > 0):
+##    if(input == 74):
+##        print("input equals 74")
+##        if(input == 0):
+##            print(0)
+##    elif(input == 73):
+##        print("input equals 73")
+##    else:
+##        print("input is > 0, but not equal to 74 or 73")
+##else:
+##    print(input)
+##
+##x = 3
+##y = -6
+##if x > 0 or y > 0:
+##    print("not weird :)")
+##else:
+##    print("something is weird")
+##
+##if x > 0:
+##    print("not weird :)")
+##elif y > 0:
+##    print("not weird :)")
+##else:
+##    print("something is weird")
+##    
+##
+##
+##
+##
+##
+##
+##
+his_name = "Kinshuk"
+
+print(his_name)
+print(len(his_name))
+
+
+##print("Vedi said Kinshuk's name was 8 letters long")
+
+
+# 4 types of data - int, float, string, boolean
+
+y = "32"
+
+
+print(int(y) + 3)
+
+
+#int(), float(), str()
+
+
+z = 5
+print(str(z) + "3")
+
+
+
+#58i82w0
